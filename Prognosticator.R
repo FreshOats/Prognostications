ab96ebf6c34c75d719ed8ad59d2a7cb5f8cafa18
Justin Papreck <33167541+FreshOats@users.noreply.github.com>
2019-02-11 12:04:12 -0800

    Update Prognosticator.R
    This was a major remodel of the functionality and processing of the Prognosticator app. Initially the function was only returning the probabilities and results associated with the highest found ngram. This fixes that and returns the sequentially smaller ngrams with the associated adjusted probabilities, as described in the Stupid Backoff model. 
      Some of the simplicity of running the program through a few smaller functions was sacrificed to ensure that the backoff process was being used correctly. 
      In the Pipeline, many of the headings were changed to be capitalized, so that the output would be easier to process and not have to make back-end changes. The probabilities are now represented as percents, rather than decimals, which makes it easier for the user to see and understand. 
      Another major fix was the issue where the program crashed if an unknown word was passed as the last word in a string. Now, instead of producing an error, the app will scan backwards through the word and look for the largest letter combination. While not ideal, this at least provides a prediction instead of throwing up an error. This can be useful, particularly when the last word of the text is simply misspelled. 
    
    - Changed predictions to return as 1/1000 th of a percent and with a % symbol
    - Fixed a problem where the program would fail when an unknown word was entered as the last word in the text for prediction. Now it scans unknown words backward for the largest known set of combined letters. 
    - Changed the working directory to save to and source from the app folder to be deployed on ShinyApps

--- Prognosticator.R
+++ Prognosticator.R
@@ -5,43 +5,115 @@ library(stringr)
 library(dplyr)
 
 ## Open the file that has the n-grams and their associated probabilites
-nGrams <- fread(paste0(path, "filtered.txt"), col.names = c("phrase", "prediction", "probability"), data.table = T, stringsAsFactors = F, encoding = "UTF-8")
+nGrams <- fread("filtered.txt", col.names = c("Phrase", "Prediction", "Probability"), data.table = T, stringsAsFactors = F, encoding = "UTF-8")
     lambda <- 0.4
-    setkey(nGrams, phrase)
+    setkey(nGrams, Phrase)
 
-## This determines whether the input string exists within the file ngrams in the filtered text
-nTokens <- function(input.Phrase) {
-    if (input.Phrase == "") return(0)
-    return(str_count(input.Phrase, "_") + 1)
-}
 
-## This cuts off the first word from the input string. If the string is of length 1, then it returns an NA
-nGram.Axe <- function(input.Phrase) {
-    if (nTokens(input.Phrase) == 1) return("<NA>")
-    return(sub("^[^_]*_", "", input.Phrase))
-}
-
-## This tokenizes the input string - the highest number of words that are searched is 6, since there is nothing greater than 5-grams, this is more than ample. The else condition lowers the number incrementally
-input.Axe <- function(input.String, n = 6) {
-    if (trimws(input.String, "both") == "") return ("<NA>")
-    chops <- tokens(input.String, what = "word", remove_numbers = T, remove_punct = T, remove_symbols = T, remove_twitter = T, remove_hyphens = T, remove_url = T, remove_separators = T, ngrams = n) %>% 
+## This tokenizes the input string - the highest number of words that are searched is 4, since the largest n-gram sampled was a 5-gram, but the fifth word is stripped from the reference phrase, the largest is should sample is a 4-gram. The else condition lowers the number incrementally, removing the first word from the input string until there are only the last 4 words remaining.
+input.Axe <- function(input.String, n = 4) {
+    chops <- tokens(input.String, what = "word", remove_numbers = T, remove_punct = T, remove_symbols = T, 
+                    remove_twitter = T, remove_hyphens = T, remove_url = T, remove_separators = T, ngrams = n) %>% 
         tokens_tolower() %>%
         unlist(use.names = F) %>%
         as.data.table()
     if (chops[, .N] > 0) return(chops[.N])
     else return(input.Axe(input.String, n - 1))
+    }
+
+  
+# This now determines the number of words in the input string by counting the spaces between words. If the number of tokens is exactly 1, then the function looks for any characters from the back of each word to find the nearest match. Otherwise it searches for strings that do not start with an underscore but end with an underscore and subsequently remove the final underscore from the input phrase.     
+nGram.Axe <- function(input.Phrase) {
+    if (input.Phrase == " ") return(0)
+    else {nTokens <- str_count(input.Phrase, "_") + 1} 
+    
+    if (nTokens == 1) return(sub(".", "",input.Phrase))
+    else return(sub("^[^_]*_", "", input.Phrase))
+    }
+
+
+## The Stupid Backoff looks at the input string and scans the n-gram table for matching strings. If a result is returned, the model returns the indexed matching results and scales the probability based on the next lower n-gram. If there is no reult returned, the string is returned to the Axe, which then reduces the ngram by 1. This is based off the Stupid Backoff Model. 
+Backoff <- function(input.Phrase, lambda = 0.4) {
+    result <- nGrams[Phrase == input.Phrase]
+    nTokens <- str_count(input.Phrase, "_") + 1
+    ## Four-Grams
+    if (nrow(result) > 0 & nTokens == 4) {
+        nResult <- mutate(result, Probability = 100 * Probability)
+        
+        if (nrow(nResult) < 8) {
+            input.Phrase <- nGram.Axe(input.Phrase)
+            lessone.Result <- mutate(nGrams[Phrase == input.Phrase], Probability = 100 * lambda * Probability)
+            lessone.Result <- lessone.Result[!(lessone.Result$Prediction %in% nResult$Prediction) ,]
+            nResult <- rbind(nResult, lessone.Result)
+        }
+        
+        if (nrow(nResult) < 8) {
+            input.Phrase <- nGram.Axe(input.Phrase)
+            lesstwo.Result <- mutate(nGrams[Phrase == input.Phrase], Probability = 100 * lambda^2 * Probability)
+            lesstwo.Result <- lesstwo.Result[!(lesstwo.Result$Prediction %in% nResult$Prediction) ,]
+            nResult <- rbind(nResult, lesstwo.Result)
+        }
+        
+        if (nrow(nResult) < 8) {
+            input.Phrase <- nGram.Axe(input.Phrase)
+            lessthree.Result <- mutate(nGrams[Phrase == input.Phrase], Probability = 100 * lambda^3 * Probability)
+            lessthree.Result <- lessthree.Result[!(lessthree.Result$Prediction %in% nResult$Prediction) ,]
+            nResult <- rbind(nResult, lessthree.Result)
+        }
+        
+        return(nResult)
+    }
+    
+    ## Three Grams
+    else if (nrow(result) > 0 & nTokens == 3) {
+        nResult <- mutate(result, Probability = 100 * Probability)
+        
+        if (nrow(nResult) < 8) {
+            input.Phrase <- nGram.Axe(input.Phrase)
+            lessone.Result <- mutate(nGrams[Phrase == input.Phrase], Probability = 100 * lambda * Probability)
+            lessone.Result <- lessone.Result[!(lessone.Result$Prediction %in% nResult$Prediction) ,]
+            nResult <- rbind(nResult, lessone.Result)
+        }
+        
+        if (nrow(nResult) < 8) {
+            input.Phrase <- nGram.Axe(input.Phrase)
+            lesstwo.Result <- mutate(nGrams[Phrase == input.Phrase], Probability = 100 * lambda^2 * Probability)
+            lesstwo.Result <- lesstwo.Result[!(lesstwo.Result$Prediction %in% nResult$Prediction) ,]
+            nResult <- rbind(nResult, lesstwo.Result)
+        }
+        
+        return(nResult)
+    }
+    
+    ## Two Grams
+    else if (nrow(result) > 0 & nTokens == 2) {   
+        nResult <- mutate(result, Probability = 100 * Probability)
+        
+        if (nrow(nResult) < 8) {
+            input.Phrase <- nGram.Axe(input.Phrase)
+            lessone.Result <- mutate(nGrams[Phrase == input.Phrase], Probability = 100 * lambda * Probability)
+            lessone.Result <- lessone.Result[!(lessone.Result$Prediction %in% nResult$Prediction) ,]
+            nResult <- rbind(nResult, lessone.Result)
+        }
+        return(nResult)
+    }
+    
+    ## One Grams
+    else if (nrow(result) > 0 & nTokens == 1) {   
+        nResult <- mutate(result, Probability = 100 * Probability)
+        return(nResult)
+    } 
+        
+        
+    else return(Backoff(nGram.Axe(input.Phrase)))
 }
 
-## The Stupid Backoff looks at the input string and scans the n-gram table for matching strings. If a result is returned, the model returns the indexed matching results and scales the probability based on a. If there is no reult returned, the string is returned to the Axe, which then reduces the ngram by 1. This is based off the Stupid Backoff Model 
-Backoff <- function(input.Phrase, a = 1) {
-    result <- nGrams[phrase == input.Phrase]
-    if (nrow(result) > 0) return(mutate(result, probability = a * probability))
-    else return(Backoff(nGram.Axe(input.Phrase), a * lambda))
-}
 
 ## This is the actual predictor for the front end. The string is entered as an input. This is then tokenized as a decreasing series of ngrams. After the tokenization, the ngram is passed to the Backoff function, which searches for matches and then incrementally backed off and returns predictions based on the highest ngram
 Prognosticate <- function(input.String) {
     nGram <- input.Axe(input.String)
     prognostication <- Backoff(nGram)
+    prognostication$Probability <- round(prognostication$Probability, digits = 3)
+    prognostication$Probability <- paste0(prognostication$Probability, " %")
     return(prognostication)
 }
